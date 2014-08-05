75c427afdeff866f6890df6bd6ba7d65171b2f38
SimonHFrost <simonhfrost@gmail.com>
2014-08-05 16:41:29 +0200

    make button play the track that has been assigned to it

--- script.js
+++ script.js
@@ -1,18 +1,17 @@
 var Jazz = document.getElementById("Jazz1"); if(!Jazz || !Jazz.isJazz) Jazz = document.getElementById("Jazz2");
-Jazz.MidiInOpen(0,function(t,a){
-  // Jazz.MidiOutLong(a);
-  $(".square").eq(2).trigger( "click" );
-});
+var currentSound;
 
 SC.initialize({
   client_id: "6603d805dad113c51b7df28b6737f2cc",
   redirect_uri: "http://example.com/callback.html",
 });
 
-var currentSound;
-$(".square").click(function() {
-  var track = $(this).attr("url");
-  var position = $(this).attr("position");
+Jazz.MidiInOpen(0,function(t,a){
+  var square = $(".square" + a[1]);
+  console.debug($(square));
+
+  var track = $(square).attr("url");
+  var position = $(square).attr("position");
   SC.stream(track, function(sound){
     if (currentSound) {
       currentSound.stop();
