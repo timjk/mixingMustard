2fd4854f15b263551ceb4bcd9107465e6762c94e
SimonHFrost <simonhfrost@gmail.com>
2014-08-05 22:01:30 +0200

    toggle armed image on currently playing square

--- midiSequencing.js
+++
@@ -1,2 +0,0 @@
-var Jazz = document.getElementById("Jazz1"); if(!Jazz || !Jazz.isJazz) Jazz = document.getElementById("Jazz2");
-Jazz.MidiInOpen(0,function(t,a){ Jazz.MidiOutLong(a);});
--- script.js
+++ script.js
@@ -10,8 +10,27 @@ Jazz.MidiInOpen(0,function(t,a){
   var square = $(".square" + a[1]);
   console.debug($(square));
 
-  var track = $(square).attr("url");
-  var position = $(square).attr("position");
+  markSquareAsArmed(square);
+  playTrackOnSquare(square);
+});
+
+$("[class^=square]").click(function() {
+  markSquareAsArmed($(this));
+  playTrackOnSquare($(this));
+});
+
+function markSquareAsArmed(square) {
+  square.attr("src", "./img/button-armed.png");
+
+  var otherSquares = $("[class^=square]").not(square);
+  $.each(otherSquares, function(i, val) {
+    $(val).attr("src", "./img/button-passive.png");
+  });
+}
+
+function playTrackOnSquare(square) {
+  var track = square.attr("url");
+  var position = square.attr("position");
   SC.stream(track, function(sound){
     if (currentSound) {
       currentSound.stop();
@@ -20,4 +39,17 @@ Jazz.MidiInOpen(0,function(t,a){
     sound.setPosition(position);
     sound.play();
   });
+}
+
+$(function() {
+  $("[class^=square]")
+    .mouseover(function() {
+      src = "./img/button-armed.png";
+      $(this).attr("src", src);
+    })
+
+    .mouseout(function() {
+      src = "./img/button-active.png";
+      $(this).attr("src", src);
+    });
 });
